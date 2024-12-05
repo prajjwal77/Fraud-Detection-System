8dbabf0ca32dd8c1d052d8bae8cd427a649b0b27
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2024-12-05 23:25:18 +0530

    Add files via upload

---
+++ FraudDetectionSystem/src/test/java/com/FraudDetectionSystem/FraudDetectioSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FraudDetectionSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectioSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
